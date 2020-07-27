7be24cff0df364688b1806e7c2f290c3bb51c48f
Lyndon-Wu <lyndon.wu@qq.com>
2020-07-27 08:56:06 +0000

    Exercise 1.

--- theia-training/src/browser/theia-training-frontend-contribution.ts
+++ theia-training/src/browser/theia-training-frontend-contribution.ts
@@ -1,15 +1,47 @@
 import { injectable, inject } from "inversify";
 import URI from "@theia/core/lib/common/uri";
 import { CommandContribution, CommandRegistry, MenuContribution, MenuModelRegistry } from "@theia/core/lib/common";
-import { open, KeybindingContribution, KeybindingRegistry, FrontendApplicationContribution, QuickOpenService, Endpoint, QuickOpenItem, QuickOpenMode, StatusBar, QuickOpenContribution, QuickOpenHandlerRegistry, OpenerService } from "@theia/core/lib/browser";
+
+import { CommonMenus, StatusBarEntry, StatusBar } from "@theia/core/lib/browser";
+import { KeybindingContribution, KeybindingRegistry } from "@theia/core/lib/browser";
+import {  QuickOpenModel, QuickOpenItem, QuickOpenOptions, QuickOpenMode } from "@theia/core/lib/browser";
+import { QuickOpenHandler, QuickOpenService, QuickOpenContribution, QuickOpenHandlerRegistry } from "@theia/core/lib/browser";
+import { open, FrontendApplicationContribution, Endpoint, OpenerService } from "@theia/core/lib/browser";
+
 import { WorkspaceService } from "@theia/workspace/lib/browser";
 
+
+export const OpenQuickFileCommand = {
+    id: 'open.quick.file.command',
+    label: "Open Quick File..."
+};
+
+export enum StatusBarAlignment {
+    LEFT, RIGHT
+};
+
+export class StatusBarItem implements StatusBarEntry {
+    text: string;
+    alignment: StatusBarAlignment;
+    color?: string;
+    className?: string;
+    tooltip?: string;
+    command?: string;
+    arguments?: any[];
+    priority?: number;
+    onclick?: (e: MouseEvent) => void;
+};
+
 @injectable()
-export class TheiaTrainingFrontendContribution implements CommandContribution, MenuContribution, KeybindingContribution, FrontendApplicationContribution, QuickOpenContribution {
+export class TheiaTrainingFrontendContribution implements QuickOpenHandler, CommandContribution, MenuContribution, KeybindingContribution, FrontendApplicationContribution, QuickOpenContribution {
 
     readonly prefix = 'file';
+    readonly description = 'Quick File';
+    readonly items: QuickOpenItem[];
 
-    readonly description = 'Quick File'
+
+    @inject(StatusBar)
+    protected readonly statusBar: StatusBar;
 
     @inject(QuickOpenService)
     protected readonly quickOpenService: QuickOpenService;
@@ -20,35 +52,65 @@ export class TheiaTrainingFrontendContribution implements CommandContribution, M
     @inject(OpenerService)
     protected readonly openereService: OpenerService;
 
-    @inject(StatusBar)
-    protected readonly statusBar: StatusBar;
-
     registerCommands(registry: CommandRegistry): void {
         // TODO: Add `Open Quick File...` command.
         // Use `CommandRegistry.registerCommand` to register a new command.
         // The command should call `this.open` for the first workspace root, i.e. `this.workspaceService.tryGetRoots()[0]`.
         // if there is no workspace root then the command should not be visible and enabled.
+        registry.registerCommand(OpenQuickFileCommand, {
+            execute: () => {
+                if (this.workspaceService.tryGetRoots()[0]) {
+                    this.open('/workspace');
+                }
+            }
+        });
     }
 
     registerKeybindings(registry: KeybindingRegistry): void {
-        // TODO: Add `ctrlcmd+k f` keybinding for `Open Quick File...` command.
+        // TODO: Add `ctrlcmd+k` keybinding for `Open Quick File...` command.
         // Use `KeybindingRegistry.registerKeybinding` to register a new keybinding.
+        registry.registerKeybinding({
+            command: OpenQuickFileCommand.id,
+            context: 'Keybinding',
+            keybinding: "ctrlcmd+k"
+        });
     }
 
     registerMenus(registry: MenuModelRegistry): void {
         // TODO: Add `Open Quick File...` menu item in `CommonMenus.FILE_OPEN` menu path.
         // Use `MenuModelRegistry.registerMenuAction` to register a new menu action.
+        registry.registerMenuAction(CommonMenus.FILE_OPEN, {
+            commandId: OpenQuickFileCommand.id,
+            label: OpenQuickFileCommand.label
+        });
     }
 
     onStart(): void {
         // TODO: Add `Open Quick File...` status bar item with file icon aligned left
         // Use `StatusBar.setElement` to add a new status bar entry.
+        const statusBarItem = new StatusBarItem();
+        statusBarItem.alignment = StatusBarAlignment.LEFT;
+        statusBarItem.text = 'Status Bar Text';
+        this.statusBar.setElement(OpenQuickFileCommand.id, statusBarItem);
+    }
+
+    getModel(): QuickOpenModel {
+        return {
+            onType: (lookFor: string, acceptor: (items: QuickOpenItem[]) => void) => {
+                acceptor(this.items);
+            }
+        };
+    }
+
+    getOptions(): QuickOpenOptions {
+        return {};
     }
 
     registerQuickOpenHandlers(handlers: QuickOpenHandlerRegistry): void {
         /* BONUS: reimplement like QuickOpenHandler */
         // Use IDE features like content assist, reference navigation and hover to learn how to use `QuickOpenHandler`.
         // handlers.registerHandler(this);
+        handlers.registerHandler(this);
     }
 
     protected async open(current: string, path: string[] = []): Promise<void> {
